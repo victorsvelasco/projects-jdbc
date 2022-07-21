5b161869520f0f58f4fa1106798e650cf0d1180a
victorsvelasco <victorsvelasco@gmail.com>
2022-07-21 19:14:19 -0300

    update data

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,55 +10,27 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			//adicionando vendedores
-			/*
 			st = conn.prepareStatement(
-					"Insert INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALLUES"
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			
-			st.setString(1, "Victor Sampaio");
-			st.setString(2, "victorsvelasco@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("04/05/1994").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 2);
-			 */
-			
-			
-			//adicionando departamentos
-			st = conn.prepareStatement("insert into department (Name) values ('D1'), ('D2')", 
-					Statement.RETURN_GENERATED_KEYS);
-			
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+		
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}else {
-				System.out.println("No rown affected");
-			}
+			System.out.println("Done! Rows Affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		/*
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
-		*/
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
